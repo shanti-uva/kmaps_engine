917f78a301a9fca8f7cd92031df666a8c63f7639
Derik Ramirez <drc2r@virginia.edu>
2018-04-23 18:35:27 +0100

    Added fallback for citations and notes
    Added a fallback with the full layout for citations and notes when the
    request is for only html and then return only the markup for the xhr.

--- app/controllers/citations_controller.rb
+++ app/controllers/citations_controller.rb
@@ -1,9 +1,5 @@
 class CitationsController < ResourceController::Base
   belongs_to :caption, :description, :feature_geo_code, :feature_name, :feature_name_relation, :feature_relation, :summary
   
-  index.wants.html do
-    #TODO: if request.xhr? fallback for non js call
-    parent_object
-    render partial: 'citations/list', locals: { citations: collection }
-  end
+  index.wants.html { render(partial: 'citations/list', locals: { citations: collection }) if request.xhr? }
 end
