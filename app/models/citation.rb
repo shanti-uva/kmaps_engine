6b89cfb3a003aae3a766671c3b07a2ff7931776e
Derik Ramirez <drc2r@virginia.edu>
2018-05-03 11:40:25 +0100

    Fix bibliographical reference for Online Resources
    An error occured when a MMSIntegration citation was found and it didn't
    contain any pages, the case was not being taken into account, that was
    fixed and added some of the code for the citations to be displayed on
    the _feature partial.
    
    MANU-4932

--- app/models/citation.rb
+++ app/models/citation.rb
@@ -65,10 +65,12 @@ class Citation < ActiveRecord::Base
       pages = self.web_pages
       if pages.count==1
         source_str = pages.first.full_path
-      else
+      elsif pages.count > 1
         pages_a = pages.to_a
         e = pages_a.shift
         source_str = ([e.full_path] + a.collect(&:path)).join(', ')
+      else
+        source_str = "#{source.titles.first.title} #{(source.web_address.url)}"
       end
     else
       source_str = ([source.bibliographic_reference] + self.pages.collect(&:to_s)).join(', ') + '.'
