9c8ab7d548ccf55551fd917a81a766ad4a7a4d7e
Aimee Kvasir <yzzordorex@gmail.com>
2025-12-04 14:29:21 -0500

    MANU-7793 change error message text on summary plain_content to be more easily understood

--- app/models/summary.rb
+++ app/models/summary.rb
@@ -18,11 +18,18 @@ class Summary < ActiveRecord::Base
   has_many :imports, :as => 'item', :dependent => :destroy
   
   validates :language_id, :uniqueness => {:scope => :feature_id}
-  validates :plain_content, length: 1..750
+  validates :plain_content, length: { minimum: 1 }
+  validate :plain_content_length
   
   include KmapsEngine::IsCitable
 
   def plain_content
     content.strip_tags
+  end
+
+  private
+
+  def plain_content_length
+    errors.add(:base, "You have exceeded the summary limit of 750 characters, excluding HTML tags.") unless plain_content.length <= 750
   end
 end
